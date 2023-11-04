9cb660cca4cab03c33937c55342e93b6ed223939
syed-shafiq <149795409+syed-shafiq@users.noreply.github.com>
2023-11-04 11:45:33 +0500

    Add files via upload

---
+++ piaic assignments/assignment11/task11.ts
@@ -0,0 +1,5 @@
+let names: string[] = ["nadeem", "aslam", "mohsin", "atif"]; // Array of names
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]); // Print each person's name
+}
---
+++ piaic assignments/assignment2/task3.js
@@ -0,0 +1,6 @@
+var personName = "shafiq";
+console.log(personName.toLocaleLowerCase()); 
+console.log(personName.toLocaleUpperCase()); 
+console.log(personName.toLocaleUpperCase()); 
+console.log(personName.toLocaleLowerCase()); 
+console.log(personName.replace(/\b\w/g, function (c) { return c.toLocaleLowerCase.(); })); 
